df3071596be0a391c58ea1b723c447b649626e7e
nelson360 <nelsonomondi99@gmail.com>
2022-11-11 23:13:39 +0300

    init

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,14 +1,16 @@
 <template>
 <nav>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/AboutComponent">About</router-link><br>
-    <router-link to="/BloggComponent">blogg</router-link><br>
-    <router-link to="/MediaComponent"> media</router-link><br> 
-    <router-link to="/PlayersComponent">players</router-link><br>
-    <router-link to="/ScheduleComponent">schedule</router-link><br>
-    <router-link to="/ContactComponent">contact</router-link><br>
-</nav>
+  <ul>
+
+ 
+    <li><router-link to="/App">Home</router-link> </li>
+    <li><router-link to="/AboutComponent">About</router-link></li>
+    <li><router-link to="/BloggComponent">blogg</router-link></li>
+   <li><router-link to="/MediaComponent"> media</router-link></li>
+   <li><router-link to="/PlayersComponent">players</router-link></li>
+    <li><router-link to="/ScheduleComponent">schedule</router-link></li>
+    <li><router-link to="/ContactComponent">contact</router-link></li>
+  </ul>
 </nav>
 <RouterView />
 </template>
@@ -21,5 +23,59 @@ export default {
 }
 </script>
 
-  
-<style></style>
+
+
+<style>
+nav {
+    width: 100%;
+    line-height: 1.4em;
+}
+
+ul {
+    list-style: none;
+    display: block;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    text-align: justify;
+    margin-bottom: -1.4em;
+}
+
+ul:after {
+    content: "";
+    
+    color: rgb(20, 182, 33);
+    display: inline-block;
+    width: 100%;
+}
+
+li {
+    display: inline-block;
+    margin: 4%;
+    padding: auto;
+    font: size 25px;
+    font-style: normal;
+    color:rgb(20, 182, 33); 
+    text-decoration: none;
+
+}
+
+nav>a {
+    font-weight: bold;
+    color: #2c3e50;
+    /* &.router-link-exact-active {
+    color: #42b983;
+  } */
+}
+
+html {
+    position: relative;
+    min-height: 100%;
+}
+
+body {
+    margin-bottom: 60px;
+    /* Margin bottom by footer height */
+}
+</style>
+
