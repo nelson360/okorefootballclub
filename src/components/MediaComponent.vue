67c6d15604e5595f54aca3aff889f333ac70ee53
nelson360 <nelsonomondi99@gmail.com>
2022-11-24 04:09:50 +0300

    img

--- src/components/MediaComponent.vue
+++ src/components/MediaComponent.vue
@@ -2,6 +2,10 @@
 <p>
     Media ::::::::::::::::::::::
 </p>
+<img alt="" src="https://images.pexels.com/photos/5597396/pexels-photo-5597396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+<img alt="" src="https://images.pexels.com/photos/274422/pexels-photo-274422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+<img alt="" src="https://images.pexels.com/photos/10512973/pexels-photo-10512973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+
 <div>
 
 </div>
--- src/components/PlayersComponent.vue
+++ src/components/PlayersComponent.vue
@@ -19,19 +19,20 @@
     <div>
         Name: goalkeeper1<br>
         <br>
+        <img alt="" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
         number:<br>
 
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="/https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: goalkeeper2<br> <br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: goalkeeper3<br><br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
 </div>
 
@@ -39,37 +40,37 @@
     <div>
         Name: defender1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: defender2<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: defender3<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: defender4<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: defender5<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: defender6<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>pic
     <div>
         Name: defender7<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
 </div>
 
@@ -77,155 +78,157 @@
     <div>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
 
     </div>
     <div>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div><br>
     <div>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         number:<br>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2219801/pexels-photo-2219801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: midfield1<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
 
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name:striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/5470999/pexels-photo-5470999.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
+    </div>
+    <div>
+        Name: striker<br>
+        <a href =""></a>
+        number:<br>
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
-    </div>
-    <div>
-        Name: striker<br>
-        number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name:striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div>
         Name: striker<br>
         number:<br>
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/12002144/pexels-photo-12002144.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
     <div> coaches</div>
     <div>
         evans openda<br>Headcoach<br>
         <br>
 
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/2848984/pexels-photo-2848984.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load">
     </div>
     <div>
         christopher omolo<br> Ass.Head coach<br>
        <br>
 
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/6077793/pexels-photo-6077793.jpeg">
     </div>
     <div>
         Name: Technical coach<br>
         <br>
 
-        <img alt="image lost" src="/pic/logo.png">
+        <img alt="image lost" src="https://images.pexels.com/photos/14071176/pexels-photo-14071176.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
     </div>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdt7soKNrDujqvqewePErzz2v_EwGBZ2QPokP-qrhJTcR5nAQ/viewform?embedded=true" width="640" height="582" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </div>
 </template>
 
